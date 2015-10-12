d5ce40a5da3162988ca3e36f3c4b5f276f8404f4
Eygle <cavalec.johan@gmail.com>
2015-10-12 17:20:25 +0200

    change ant script to add local (linux)

---
+++ dbconfig-local.php
@@ -0,0 +1,8 @@
+<?php
+if(!defined('DB_HOST')){
+    define('DB_HOST', '195.154.91.110');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'Fmy7bkka');
+    define('DB_DATABASE', 'eyglebdd');
+}
+?>
